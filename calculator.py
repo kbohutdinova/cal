4ae9f0c89577d4d36d3a3b276837e0f83978dfbc
kbohutdinova <kbohutdinova@kse.org.ua>
2024-10-31 16:07:47 +0200

    Revert "1488"

--- calculator.py
+++ calculator.py
@@ -1,9 +1,7 @@
 x = int(input("Please, enter first number:"))
 y = int(input("Please, enter second number:"))
 operation = int(input("Which operation do you want to use? \n 1) Addition \n 2) Substraction \n 3) Multiplication"
-                      " \n 4) Division \n 5) Exponentation \n 6) Remainder \n 7) Bitwise or \n
-                   8) Bitwise and "))
-
+                      " \n 4) Division \n 5) Exponentation \n 6) Remainder \n 7) Bitwise and "))
 if operation == 1:
     print(x + y)
 if operation == 2:
@@ -23,10 +21,7 @@ if operation == 6:
     else:
         print(x % y)
 if operation == 7:
-    print(x | y)
-if operation == 8
     print(x & y)
-
 
 
 print( " Program has ended successfully.")
