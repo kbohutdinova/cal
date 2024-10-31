1c9bab6568ef8d226be486e802ab438d65a995e7
ysobko <y.sobko@kse.org.ua>
2024-10-31 15:48:15 +0200

    12345

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 x = int(input("Please, enter first number:"))
 y = int(input("Please, enter second number:"))
 operation = int(input("Which operation do you want to use? \n 1) Addition \n 2) Substraction \n 3) Multiplication"
-                      " \n 4) Division \n 5) Exponentation \n 6) Remainder "))
+                      " \n 4) Division \n 5) Exponentation \n 6) Remainder \n 7) Bitwise or"))
 if operation == 1:
     print(x + y)
 if operation == 2:
@@ -20,6 +20,8 @@ if operation == 6:
         print("Error, you could not devide by zero.")
     else:
         print(x % y)
+if operation == 7:
+    print(x | y)
 
 
 print( " Program has ended successfully.")
